fa491e3daa3704b831dc03d2911861d4ebb76a9b
Anik <anik@fivejack.com>
2022-08-21 14:44:12 +0600

    chore: initial upload

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const multer  = require('multer')
 const cors = require('cors');
+
 const app = express();
 const port = 4000;
+const UPLOAD_FOLDER = './uploads';
+
+// multer
+const upload = multer({ 
+    dest: UPLOAD_FOLDER
+})
 
 // middleware
 app.use(cors());
@@ -9,7 +17,11 @@ app.use(cors());
 // routes
 app.get('/', cors(), (req, res) => {
     res.send('I have receive your req');
-})
+});
+
+app.post('/upload', cors(), upload.single('file'), (req, res) => {
+    res.send('I have receive your req');
+});
 
 app.listen(port, () => {
     console.log('Listening to port 4000');
